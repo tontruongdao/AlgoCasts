5e743a977cafce908abdf54c9525dfa070e9c5de
Truong Dao <truong.dao.do@gmail.com>
2020-12-02 19:58:49 -0500

    Reverse String Completed

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,12 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    const splitString = str.split("");
+    const reverseArray = splitString.reverse();
+    const joinArray = reverseArray.join("");
+    
+    return joinArray
+}
 
 module.exports = reverse;
