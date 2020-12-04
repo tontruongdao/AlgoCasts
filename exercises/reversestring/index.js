ffb55d7a605805c6c7ce8ac12ac9009349abfd39
Truong Dao <truong.dao.do@gmail.com>
2020-12-03 20:18:08 -0500

    Added solution to reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,12 +6,34 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {
-    const splitString = str.split("");
-    const reverseArray = splitString.reverse();
-    const joinArray = reverseArray.join("");
+//1.
+// function reverse(str) {
+//     const splitString = str.split("");
+//     const reverseArray = splitString.reverse();
+//     const joinArray = reverseArray.join("");
     
-    return joinArray
+//     return joinArray
+// }
+
+//2.
+// function reverse(str) {
+//     let reversed = "";
+
+//     for (let character of str) {
+//         reversed = character + reversed;
+//     }
+//     return reversed
+// }
+
+//3.
+function reverse(str) {
+    // "reduce" takes all element of an array, and reduces to a single element
+    // It takes two argument, first one is an arrow function, and a starting initial value.
+    // The arrow function takes two argument, the first one is the initial value that is iterated every time.
+    // the second argument is the single item iterated to the initial value.
+    return str.split("").reduce((reversed, character) => {
+        return character + reversed;
+    }, "")
 }
 
 module.exports = reverse;
