1d31dddef80cdd31a521327d904ac0b88cf32629
Truong Dao <truong.dao.do@gmail.com>
2020-12-25 14:51:55 -0500

    Capitalize solved

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,58 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+// ################# Solution #1 ######################
+
+// function capitalize(str) {
+//     const words = [];
+
+//     for (let word of str.spli(' ')) {
+//         words.push(word[0].toUpperCase() + word.splice(1).toLowerCase());
+//     }
+
+//     return words.join(' ');
+// }
+
+
+// ################ Solution # 2 ######################
+
+function capitalize(str) {
+    let results = str[0].toUpperCase();
+
+    for (let i = 1; i < str.length; i++) {
+        if (str[i - 1] === ' ') {
+            results += str[i].toUpperCase();
+        } else {
+            results += str[i];
+        }
+    }
+
+    return results;
+}
+
+// ################# My solution ######################
+
+// function capitalize(str) {
+
+//     const strArray = str.split(" ");
+
+//     const tempArray = [];
+
+//     strArray.forEach((word) => {
+        
+//         const firstComponent = word[0].toUpperCase();
+//         const secondComponent = word.slice(1).toLowerCase();
+        
+//         const newWord = firstComponent + secondComponent;
+//         tempArray.push(newWord);
+        
+//         // console.log(tempArray);
+//     });
+
+//     return tempArray.join(" ");
+//     // console.log(newString);
+// }
+
+// capitalize("hello world");
 
 module.exports = capitalize;
