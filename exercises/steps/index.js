33587aba8f4b314dadf3b9a3c89a5fa05395119e
Truong Dao <truong.dao.do@gmail.com>
2021-01-02 18:47:31 -0500

    Steps completed

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,48 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// ########## Solution 2 Recursion ##########
+
+function steps(n, row=0, stair='') {
+
+    if (n === row) {
+        return;
+    }
+
+    if (n === stair.length) {
+        console.log(stair);
+        return steps(n, row+1);
+    }
+
+    if (stair.length <= row) {
+        stair += '#';
+    } else {
+        stair += ' '
+    }
+
+    steps(n, row, stair)
+}
+
+
+// ########## Solution 1 For loop ##########
+
+// function steps(n) {
+//     for (let row = 0; row < n; row++) {
+//         let stair = '';
+
+//         for (let column =0; column < n; column++) {
+//             if (column <= row) {
+//                 stair += '#';   
+//                 // console.log(stair);
+//             } else { 
+//                 stair += ' ';
+//                 // console.log(stair);
+//             }
+//         }
+//         console.log(stair);
+//     }
+// }
+
+steps(5);
 
 module.exports = steps;
