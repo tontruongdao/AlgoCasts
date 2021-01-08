4b81d63c67a257cdaa5eb870f65894c8feacdea4
Truong Dao <truong.dao.do@gmail.com>
2021-01-07 20:29:25 -0500

    Vowels completes

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,44 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+
+// ########## Solution 2 ##########
+
+function vowels(str) {
+    // "g" makes sure we do not "stop" at the first match, i
+    // 'i' makes our match insensitive
+    
+    const matches = str.match(/[aeiou]/gi);
+    
+    console.log(matches.length);
+    return matches ? matches.length : 0;
+}
+
+
+// ########## Solution 1 ##########
+
+// function vowels(str) {
+
+//     let count = 0;
+//     const checker = ['a', 'e', 'i', 'o', 'u'];
+
+//     // for ( let char of str.toLowerCase() ) {
+//     //     if (checker.includes(char)) {
+//     //         count++;
+//     //     }
+//     // }
+//     const arr = str.split('');
+
+//     arr.forEach(char => {
+//         if (checker.includes(char)){
+//             count++;
+//         };
+//     })
+
+//     console.log(count);
+//     return count;
+// }
+
+vowels("hello")
 
 module.exports = vowels;
